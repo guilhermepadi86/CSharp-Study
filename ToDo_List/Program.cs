31f93e4ed3bcc3a688f05a391ad1d7129c0c8126
guilhermepadi86 <guilherme.siqueirapadi@gmail.com>
2026-04-10 10:50:56 -0300

    To Do List em Json

---
+++ ToDo_List/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class Tarefa
+{
+    public string tarefa {get; set;}
+    public decimal tempoMedio {get; set;}
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        string caminhoArquivo = "Tarefas.json";
+
+        var ObjInicial = new Tarefa();
+
+        string jsonLinha = JsonSerializer.Serialize(ObjInicial, new JsonSerializerOptions {WriteIndented = true});
+
+        if (!File.Exists(caminhoArquivo))
+        {
+            File.WriteAllText(caminhoArquivo, jsonLinha);
+        }
+    }
+}
