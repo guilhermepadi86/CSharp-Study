9d6a3623e6cc7bae2347c04ae0c415937e236fd1
Guilherme Padilha <guilherme.siqueirapadi@gmail.com>
2026-04-22 16:40:40 -0300

    BubbleSort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+
+        int[] numerosAleatorios = new int[10];
+
+        for (int i = 0; i < numerosAleatorios.Length; i++)
+        {
+            numerosAleatorios[i] = rnd.Next(1, 20);
+        }
+        string beforeBubbleSort = string.Join(", ", numerosAleatorios);
+
+        for (int i = 0; i < numerosAleatorios.Length; i++)
+        {
+            if ((i + 1 < numerosAleatorios.Length) && (numerosAleatorios[i] > numerosAleatorios[i + 1]))
+            {
+                var aux = numerosAleatorios[i + 1];
+                numerosAleatorios[i + 1] = numerosAleatorios[i];
+                numerosAleatorios[i] = aux;
+                i = -1;
+            }
+        }
+
+        string afterBubbleSort = string.Join(", ", numerosAleatorios);
+
+        Console.WriteLine($"Array antes do BubbleSort: {beforeBubbleSort}\nArray depois do BubbleSort: {afterBubbleSort}");
+    }
+
+}
